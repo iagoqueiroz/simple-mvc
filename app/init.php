f79d2abf83bb212269d37c7fcae1f78fa8bc0ab4
Iago Queiroz <iago.queiroz@hotmail.com>
2016-09-19 00:39:11 -0300

    Added settings to display errors in development

--- app/init.php
+++ app/init.php
@@ -1,5 +1,9 @@
 <?php
 
+// Configurations for the development server
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('error_reporting', -1);
+ini_set('log_errors', 1);
 
 require_once '../vendor/autoload.php';
